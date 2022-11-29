192fe2486140aa726fc6a70ddf485c7ecc077a87
CyLelouch <forgithub.cy@gmail.com>
2022-11-29 09:52:05 -0500

    added features section. updated mobile compatibility and added a hover to the svg's

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port = 3002;
 
 app.use(express.static(__dirname + '/'));
 
 app.listen(port, ()=> {
-    console.log("Application running on port 3000.")
+    console.log("Application running on port 3002.")
 });
